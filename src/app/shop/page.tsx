605e0a7c60972a67e39aa105c1b81a0c2fca94c7
JayCode Developer <JayCode.contakt@gmail.com>
2023-12-27 07:22:37 +0100

    Remove Products component

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,10 +1,7 @@
-import Products from '@/components/products/products';
-
 export default function Shop() {
     return (
         <main>
            <section>
-                <Products />
             </section>
         </main>
     )
