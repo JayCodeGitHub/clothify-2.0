0dac7f2949f53d9a4b791ad1ea5e9c41d2547e7b
JayCode Developer <JayCode.contakt@gmail.com>
2023-12-28 07:18:26 +0100

    Add api data validation

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -7,12 +7,22 @@ export async function POST(req: any) {
   const data = await req.json();
   const email = data.email;
   const password = data.password;
-  console.log(email, password);
+
+  if (email === undefined || password === undefined) {
+    return NextResponse.json({message: 'Email or password is missing'}, { status: 400 });
+  }
+
+  const existingUser = await prisma.user.findUnique({ where: { email: email }});
+
+  if (existingUser !== null) {
+    return NextResponse.json({message: 'Email already exists'}, { status: 400 });
+  }
+  
 
   const newUser = await prisma.user.create({
     data: {
         email: email,
-        password: email,
+        password: password,
     },
   });
   return NextResponse.json({newUser}, { status: 200 });
