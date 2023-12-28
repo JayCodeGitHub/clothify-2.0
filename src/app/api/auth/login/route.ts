a06eaaaac9f548bc5366186e648adf32c5de6337
JayCode Developer <JayCode.contakt@gmail.com>
2023-12-28 07:15:02 +0100

    connect login route to database

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,9 +1,19 @@
 import { NextResponse } from "next/server";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient();
 
 export async function POST(req: any) {
   const data = await req.json();
   const email = data.email;
   const password = data.password;
   console.log(email, password);
-  return NextResponse.json({email, password}, { status: 200 });
+
+  const newUser = await prisma.user.create({
+    data: {
+        email: email,
+        password: email,
+    },
+  });
+  return NextResponse.json({newUser}, { status: 200 });
 }
