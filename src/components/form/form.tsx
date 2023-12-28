7b905086fc035e73c41c7e3d5f230d83f5b76974
JayCode Developer <JayCode.contakt@gmail.com>
2023-12-28 06:25:35 +0100

    Move form to separate component

---
+++ src/components/form/form.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useState} from 'react'
+
+export default function Form() {
+    const [form, setForm] = useState({
+      email: '',
+      password: '',
+    })
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = event.target
+      setForm({
+        ...form,
+        [name]: value,
+      })
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault()
+      console.log(form)
+    }
+    return (
+       <form onSubmit={handleSubmit} className='bg-red-500 p-2 w-60 flex flex-col gap-2'>
+          <input type="email" name='email' onChange={handleChange}/>
+          <input type="password" name='password' onChange={handleChange}/>
+          <button type="submit">Register</button>
+       </form>
+    )
+  }
+  
