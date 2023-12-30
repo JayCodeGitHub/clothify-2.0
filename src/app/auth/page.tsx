6a6a74c9eb1baffec4c2ce2693ac3e1ebb6f4c7c
JayCode Developer <JayCode.contakt@gmail.com>
2023-12-30 06:52:22 +0100

    Add conditional rendering to auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,12 +1,21 @@
 import Form from '@/components/form'
+import { cookies } from 'next/headers'
 
-export default function Auth() {    
+export default function Auth() {  
+  const cookieStore = cookies()
+  const token = cookieStore.get('token')
+  console.log(token)
   return (
     <main>
       <h1>Auth</h1>
-      <Form action="register"/>
-      <Form action="login"/>
+      {token ? (
+        <button>Logout</button>
+      ) : (
+        <>
+        <Form action="register"/>
+        <Form action="login"/>
+        </>
+      )}
     </main>
   )
 }
-  
