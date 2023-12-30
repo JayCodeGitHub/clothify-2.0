5fa48d9b425f2a18f3c3a3a9219022ccbaa9a1ac
JayCode Developer <JayCode.contakt@gmail.com>
2023-12-30 07:44:59 +0100

    Create useAuth hook

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,21 +1,11 @@
 import Form from '@/components/form'
-import { cookies } from 'next/headers'
 
 export default function Auth() {  
-  const cookieStore = cookies()
-  const token = cookieStore.get('token')
-  console.log(token)
   return (
     <main>
       <h1>Auth</h1>
-      {token ? (
-        <button>Logout</button>
-      ) : (
-        <>
         <Form action="register"/>
         <Form action="login"/>
-        </>
-      )}
     </main>
   )
 }
