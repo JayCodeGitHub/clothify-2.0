2c620e69830cd6b318ebb7a8d0fe2bd4de210b98
JayCode Developer <JayCode.contakt@gmail.com>
2023-12-30 06:12:22 +0100

    Change components files structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import NavBar from "@/components/navbar/navbar"
+import NavBar from "@/components/navbar"
 import { cookies } from 'next/headers'
 
 const inter = Inter({ subsets: ['latin'] })
