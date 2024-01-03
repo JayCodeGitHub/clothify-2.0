451d4c9298cc93aa0c17b83cc409067c0aa0faf4
JayCode Developer <JayCode.contakt@gmail.com>
2024-01-03 07:28:08 +0100

    Integrate navbar with navlink

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,19 +1,17 @@
 "use client"
 
-import Link from "next/link";
-import { usePathname } from 'next/navigation';
 import { useAuth } from "@/hooks/useAuth"
+import NavLink from "./navlink";
 
 export default function NavBar() {
     const { token } = useAuth();
-    const currentRoute = usePathname();
     return (
         <nav>
-            <Link href="/" className={currentRoute == "/" ? "text-red-500" : ""}>Home</Link>
-            <Link href="/cart" className={currentRoute == "/cart" ? "text-red-500" : ""}>Cart</Link>
-            <Link href="/about" className={currentRoute == "/about" ? "text-red-500" : ""}>About</Link>
-            <Link href="/shop" className={currentRoute == "/shop" ? "text-red-500" : ""}>Shop</Link>
-            {token === false ? <Link href="/auth" className={currentRoute == "/auth" ? "text-red-500" : ""}>Auth</Link> : <Link href="/profile" className={currentRoute == "/profile" ? "text-red-500" : ""}>Profile</Link>}
+            <NavLink href="/">Home</NavLink>
+            <NavLink href="/cart">Cart</NavLink>
+            <NavLink href="/about">About</NavLink>
+            <NavLink href="/shop">Shop</NavLink>
+            {token === false ? <NavLink href="/auth">Auth</NavLink> : <NavLink href="/profile">Profile</NavLink>}
       </nav>
     )
 }
--- src/components/navlink.tsx
+++ src/components/navlink.tsx
@@ -1,7 +1,16 @@
 "use client"
 
-export default function NavLink() {
+import Link from "next/link";
+import { usePathname } from 'next/navigation';
+
+export default function NavLink({
+    children, href
+  }: {
+    children: React.ReactNode
+    href: string
+  }) {
+    const currentRoute = usePathname();
     return (
-       <h1>NavLink</h1>
+        <Link href={href} className={currentRoute == href ? "text-red-500" : ""}>{children}</Link>
     )
 }
