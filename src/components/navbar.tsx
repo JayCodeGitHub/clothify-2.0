66a2817fbadd262dc120070b3829037292bff3aa
JayCode Developer <JayCode.contakt@gmail.com>
2024-01-03 07:18:44 +0100

    Add simple active class name to navbar links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,17 +1,19 @@
 "use client"
 
-import { useAuth } from "@/hooks/useAuth"
 import Link from "next/link";
+import { usePathname } from 'next/navigation';
+import { useAuth } from "@/hooks/useAuth"
 
 export default function NavBar() {
     const { token } = useAuth();
+    const currentRoute = usePathname();
     return (
         <nav>
-            <Link href="/">Home</Link>
-            <Link href="/cart">Cart</Link>
-            <Link href="/about">About</Link>
-            <Link href="/shop">Shop</Link>
-            {token === false ? <Link href="/auth">Auth</Link> : <Link href="/profile">Profile</Link>}
+            <Link href="/" className={currentRoute == "/" ? "text-red-500" : ""}>Home</Link>
+            <Link href="/cart" className={currentRoute == "/cart" ? "text-red-500" : ""}>Cart</Link>
+            <Link href="/about" className={currentRoute == "/about" ? "text-red-500" : ""}>About</Link>
+            <Link href="/shop" className={currentRoute == "/shop" ? "text-red-500" : ""}>Shop</Link>
+            {token === false ? <Link href="/auth" className={currentRoute == "/auth" ? "text-red-500" : ""}>Auth</Link> : <Link href="/profile" className={currentRoute == "/profile" ? "text-red-500" : ""}>Profile</Link>}
       </nav>
     )
 }
