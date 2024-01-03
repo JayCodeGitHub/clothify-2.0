04ae0cfb5e7fa43a9806bd13c1cce1288452c5fb
JayCode Developer <JayCode.contakt@gmail.com>
2024-01-03 12:13:06 +0100

    Change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,14 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Clothify',
   description: 'Clothify is a clothing store project created for my developer portfolio',
+  icons: {
+    icon: [
+      {
+        url: "/images/favicon.svg", 
+        href: "/images/favicon.svg", 
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
