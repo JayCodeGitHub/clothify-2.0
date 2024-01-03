4ea56ad0de05686df34a4066b22a7b9e37e73eef
JayCode Developer <JayCode.contakt@gmail.com>
2024-01-03 07:34:07 +0100

    Create navigationItems

---
+++ src/items/navigationItems.ts
@@ -0,0 +1,10 @@
+export const NavigationItems = {
+    list: [
+        { name: "Notes", href: "/notes" },
+        { name: "Todos", href: "/todo" },
+        { name: "Sites", href: "/sites" },
+        { name: "About", href: "/about" },
+    ],
+    auth: {name: "Auth", href: "/auth"},
+    profile: {name: "Profile", href: "/profile"},
+};
