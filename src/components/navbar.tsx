f622528447f52794597e4cd32756f6d8a72a28dc
JayCode Developer <JayCode.contakt@gmail.com>
2024-01-03 15:27:22 +0100

    Add cart icon

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -21,7 +21,12 @@ export default function NavBar() {
                     {token === false ? <NavLink href={auth.href}>{auth.name}</NavLink> : <NavLink href={profile.href}>{profile.name}</NavLink>}
                 </li>
                 <li>
-                    <NavLink href={cart.href}>{cart.name}</NavLink>
+                    <NavLink href={cart.href}>
+                        <span className="flex justify-center items-center flex-col">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M6 2L3 6v14c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2V6l-3-4H6zM3.8 6h16.4M16 10a4 4 0 1 1-8 0"/></svg>
+                            {cart.name}
+                        </span>
+                    </NavLink>
                 </li>
             </ul>  
       </nav>
