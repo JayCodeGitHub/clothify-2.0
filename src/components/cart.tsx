65293dfb986ba8d546ec054b88c816d658bbcfaa
JayCode Developer <JayCode.contakt@gmail.com>
2024-01-08 11:32:34 +0100

    Style cart and add it to navbar

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -1,13 +1,27 @@
 interface CartProps {
     isCart: boolean;
-    setIsCart: (a: boolean) => void;
+    toggleCart: () => void;
   }
 
-export default function Cart({ isCart, setIsCart }: CartProps) {
+export default function Cart({ isCart, toggleCart }: CartProps) {
     return (
-        <div>
-            <div/>
-            <section>
+        <div className="fixed z-50">
+            <div 
+                onClick={toggleCart}
+                className="fixed top-0 left-0 z-30 w-full h-full"
+                style={{ 
+                    backgroundColor: `${ isCart ? 'rgba(0, 0, 0, 0.5)' : 'rgba(0, 0, 0, 0)'}`,
+                    visibility: `${ isCart ? 'visible' : 'hidden'}`,
+                    transition: 'all .3s .1s ease-in-out'
+                }}
+            />
+            <section 
+                className="flex fixed flex-col items-center gap-8 top-0 right-0 py-8 px-0 w-[80%] md:w-[35%] h-screen overflow-auto z-40 bg-white"
+                style={{ 
+                    transform: `${ isCart ? 'translateX(0)' : 'translateX(+100%)'}`,
+                    transition: 'transform .3s .1s ease-in-out',
+                }}
+            >
                 <div></div>
                 <div></div>
                 <p>Subtotal:</p>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,70 +1,77 @@
 "use client"
 
+import { useState } from "react";
 import { useAuth } from "@/hooks/useAuth"
 import NavLink from "./navlink";
 import { NavigationItems } from "@/items/navigationItems";
 import MobileMenu from "./mobileMenu";
+import Cart from "./cart";
 
 export default function NavBar() {
     const { token } = useAuth();
+    const [isCart, setIsCart] = useState(false);
     const { auth, profile, cart } = NavigationItems;
+
+    const toggleCart = () => {
+        setIsCart(prevValue => !prevValue);
+    }
+
 
     return (
         <nav className="w-full">
             <MobileMenu/>
             <div  className="w-full h-24 md:flex justify-between items-center 2xl:px-44 xl:px-28 px-4 hidden ">
-            <ul className="flex justify-center items-center gap-6">
-                {NavigationItems.list.map(({href, name}) => (
-                    <li key={name}>
-                        <NavLink href={href}>{name}</NavLink>    
+                <ul className="flex justify-center items-center gap-6">
+                    {NavigationItems.list.map(({href, name}) => (
+                        <li key={name}>
+                            <NavLink href={href}>{name}</NavLink>    
+                        </li>
+                    ))}
+                </ul>
+                <ul className="flex justify-center items-center gap-12">
+                    <li>
+                        <NavLink href={`${token === false ? auth.href : profile.href}`}>
+                            <span className="flex justify-center items-center flex-col">
+                                <svg 
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                >
+                                    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"/>
+                                    <circle cx="12" cy="7" r="4"/>
+                                </svg>
+                                {token === false ? auth.name : profile.name}
+                            </span>
+                        </NavLink>
                     </li>
-                ))}
-            </ul>
-            <ul className="flex justify-center items-center gap-12">
-                <li>
-                    <NavLink href={`${token === false ? auth.href : profile.href}`}>
-                        <span className="flex justify-center items-center flex-col">
+                    <Cart isCart={isCart} toggleCart={toggleCart}/>
+                    <li onClick={toggleCart} className="cursor-pointer hover:text-primary transition-all">
+                            <span className="flex justify-center items-center flex-col">
                             <svg 
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
+                                xmlns="http://www.w3.org/2000/svg" 
+                                width="24" 
+                                height="24" 
+                                viewBox="0 0 24 24" 
+                                fill="none" 
+                                stroke="currentColor" 
+                                strokeWidth="2" 
+                                strokeLinecap="round" 
                                 strokeLinejoin="round"
                             >
-                                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"/>
-                                <circle cx="12" cy="7" r="4"/>
+                                <path 
+                                    d="M6 2L3 6v14c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2V6l-3-4H6zM3.8 6h16.4M16 10a4 4 0 1 1-8 0"
+                                />
                             </svg>
-                            {token === false ? auth.name : profile.name}
-                        </span>
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink href={cart.href}>
-                        <span className="flex justify-center items-center flex-col">
-                        <svg 
-                            xmlns="http://www.w3.org/2000/svg" 
-                            width="24" 
-                            height="24" 
-                            viewBox="0 0 24 24" 
-                            fill="none" 
-                            stroke="currentColor" 
-                            strokeWidth="2" 
-                            strokeLinecap="round" 
-                            strokeLinejoin="round"
-                        >
-                            <path 
-                                d="M6 2L3 6v14c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2V6l-3-4H6zM3.8 6h16.4M16 10a4 4 0 1 1-8 0"
-                            />
-                        </svg>
-                            {cart.name}
-                        </span>
-                    </NavLink>
-                </li>
-            </ul>  
+                                {cart.name}
+                            </span>
+                    </li>
+                </ul>  
             </div>
       </nav>
     )
