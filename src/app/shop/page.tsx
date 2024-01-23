ad6116dd8b5348f81bc2021268397ca19ee6de40
JayCode Developer <JayCode.contakt@gmail.com>
2024-01-23 07:08:51 +0100

    Conect shop page with cms

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,11 +1,30 @@
-export default function Shop() {
+import { performRequest } from '../../lib/datocms';
+
+const PAGE_CONTENT_QUERY = `
+  query Shop {
+    allItems {
+      id
+      title
+      _status
+      _firstPublishedAt
+    }
+  
+    _allItemsMeta {
+      count
+    }
+  }`;
+
+export default async function Shop() {
+    const { data: { allItems } } = await performRequest({ query: PAGE_CONTENT_QUERY });
     return (
         <main>
            <section>
             <h1>Shop</h1>
+            {allItems.map(({ id, title}: {title: string, id: string}) => (
+                    <h2 key={id}>{title}</h2>
+                )
+            )}
             </section>
         </main>
     )
 }
-  
-
