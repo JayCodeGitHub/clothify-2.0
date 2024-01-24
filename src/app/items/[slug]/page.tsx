2d538bf1998d25abafa742595e6098dcf0cd640c
JayCode Developer <JayCode.contakt@gmail.com>
2024-01-24 06:21:21 +0100

    Add all elements from cms to item page

--- src/app/items/[slug]/page.tsx
+++ src/app/items/[slug]/page.tsx
@@ -28,6 +28,11 @@ export default async function Page({ params }: { params: { slug: string} }) {
     query Shop {
         allItems(filter: {slug: {eq: "${params.slug}"}}) {
           title
+  				description
+  				price
+  				productType
+  				sizes
+  				thumbnailAlt
           thumbnail {
             responsiveImage(imgixParams: {w: 800, h: 1200}) {
               src
@@ -35,17 +40,32 @@ export default async function Page({ params }: { params: { slug: string} }) {
               height
             }
           }
+  				gallery {
+            id
+         		responsiveImage(imgixParams: {w: 800, h: 1200}) {
+              	src
+              	width
+              	height
+            }
+      		}
       }
     }`;
 
       const { data: { allItems } } = await performRequest({ query: ITEM_CONTENT_QUERY });
       const items = allItems[0];
-      const {title, thumbnail} = items;
+      const {title, description, price, sizes, thumbnailAlt, thumbnail, gallery} = items;
 
     return (
         <div>
             <h1>{title}</h1>
-            <Image src={thumbnail.responsiveImage.src} width={thumbnail.responsiveImage.width} height={thumbnail.responsiveImage.height} alt={title}/>
+            <h2>{description}</h2>
+            <h3>{price}</h3>
+            <h4>{sizes}</h4>
+            <Image src={thumbnail.responsiveImage.src} width={thumbnail.responsiveImage.width} height={thumbnail.responsiveImage.height} alt={thumbnailAlt}/>
+            {gallery.map(({id, responsiveImage}: {id: string, responsiveImage: {src: string, width: number, height: number}}) => (
+                <Image key={id} src={responsiveImage.src} width={responsiveImage.width} height={responsiveImage.height} alt={thumbnailAlt} className='w-52'/>
+            )
+            )}
         </div>
     )
   }
