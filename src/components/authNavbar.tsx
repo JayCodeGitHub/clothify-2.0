7c330e9193ff03da1403ecc8be7d4f2bfcb40652
JayCode Developer <JayCode.contakt@gmail.com>
2024-01-31 07:37:04 +0100

    Animate authNavBar

--- src/components/authNavbar.tsx
+++ src/components/authNavbar.tsx
@@ -2,6 +2,8 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
 
 
 const navigationItems = [
@@ -10,18 +12,33 @@ const navigationItems = [
 ]
 
 export default function AuthNavBar() {  
-const currentRoute = usePathname();
+    const currentRoute = usePathname();
+    const [activeTab, setActiveTab] = useState(navigationItems[0].href)
 
-console.log(currentRoute)
   return (
-        <span className='flex justify-around w-80 py-2'>
+        <span className='w-80 py-2 px-4'>
+            <nav className='flex w-full justify-around'>
             {navigationItems.map(({name, href}: {name: string, href: string}) => (
-                <Link key={name} href={href}  className={`hover:text-gray-500 relative ${
-                    currentRoute === href ? "bg-primary" : ""
-                } rounded-full px-3 py-1.5 font-medium outline-sky-400 transition focus-visible:outline-2`}>
-                    {name}
+                <Link 
+                    key={href} 
+                    href={href}  
+                    onClick={() => setActiveTab(href)}
+                    className={`
+                        ${currentRoute === href ? "text-white" : "text-dark hover:text-gray-500" }
+                        relative px-3 py-1.5 font-medium outline-sky-400 transition focus-visible:outline-2
+                    `}>
+                        {currentRoute === href && (
+                            <motion.div 
+                                layoutId='active-tab'
+                                className='absolute inset-0 bg-primary'
+                                style={{ borderRadius: 9999 }}
+                                transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
+                            />
+                        )}
+                        <span className='relative z-10'>{name}</span>
                 </Link>
             ))}
+            </nav>
         </span>
   )
 }
