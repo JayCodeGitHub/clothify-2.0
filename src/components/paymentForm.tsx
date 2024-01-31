23a2000cdbb71a3172d2435a2f29ef2bef4bfc0a
JayCode Developer <JayCode.contakt@gmail.com>
2024-01-31 10:00:00 +0100

    Fix paymentForm inputs style

--- src/components/paymentForm.tsx
+++ src/components/paymentForm.tsx
@@ -3,20 +3,20 @@ export default function PaymentForm() {
         <form className="flex flex-col gap-2">
              <span className='flex flex-col w-full px-2'>
                 <label className='text-sm self-start'>{`Cardholder's Name`}</label>
-                <input type="text" placeholder="Full Name" name="card-name" className="w-full h-12 px-4 border border-gray-300 rounded-md my-2" />
+                <input type="text" placeholder="Full Name" name="card-name" className="w-full p-3 border border-gray-300 rounded-md my-2" />
              </span>
              <span className='flex flex-col w-full px-2'>
              <label className='text-sm self-start'>Card Number</label>
-                <input type="text" placeholder="---- ---- ---- ----" name="card-number" className="w-full h-12 px-4 border border-gray-300 rounded-md my-2" />
+                <input type="text" placeholder="---- ---- ---- ----" name="card-number" className="w-full p-3 border border-gray-300 rounded-md my-2" />
              </span>
              <span className="flex">
                 <span className='flex flex-col w-1/2 px-2'>
                     <label className='text-sm self-start'>Expiry Date</label>
-                    <input type="text" placeholder="MM/YY" name="card-date" className="w-full h-12 px-4 border border-gray-300 rounded-md my-2" />
+                    <input type="text" placeholder="MM/YY" name="card-date" className="w-full p-3 border border-gray-300 rounded-md my-2" />
                 </span>
                 <span className='flex flex-col w-1/2 px-2'>
                     <label className='text-sm self-start'>CVV</label>
-                    <input type="text" placeholder="CVV" name="card-cvv" className="w-full h-12 px-4 border border-gray-300 rounded-md my-2" />
+                    <input type="text" placeholder="CVV" name="card-cvv" className="w-full p-3 border border-gray-300 rounded-md my-2" />
                 </span>
              </span>
         </form>
