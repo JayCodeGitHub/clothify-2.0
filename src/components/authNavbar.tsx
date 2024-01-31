3a2f468f7829aa667293962e127097705bd082b0
JayCode Developer <JayCode.contakt@gmail.com>
2024-01-31 06:58:06 +0100

    Reafctore authNavBar

--- src/components/authNavbar.tsx
+++ src/components/authNavbar.tsx
@@ -1,6 +1,13 @@
 "use client"
+
 import Link from 'next/link'
 import { usePathname } from 'next/navigation';
+
+
+const navigationItems = [
+    { name: 'Login', href: '/auth/login' },
+    { name: 'Register', href: '/auth/register' },
+]
 
 export default function AuthNavBar() {  
 const currentRoute = usePathname();
@@ -8,16 +15,13 @@ const currentRoute = usePathname();
 console.log(currentRoute)
   return (
         <span className='flex justify-around w-80 py-2'>
-            <Link href='/auth/login'  className={`hover:text-gray-500 relative ${
-                currentRoute === '/auth/login' ? "bg-primary" : ""
-            } rounded-full px-3 py-1.5 font-medium outline-sky-400 transition focus-visible:outline-2`}>
-                Login
-            </Link>
-            <Link href='/auth/register' className={`hover:text-gray-500 relative ${
-                currentRoute === '/auth/register' ? "bg-primary" : ""
-            } rounded-full px-3 py-1.5 font-medium outline-sky-400 transition focus-visible:outline-2`}>
-                Register
-            </Link>
+            {navigationItems.map(({name, href}: {name: string, href: string}) => (
+                <Link key={name} href={href}  className={`hover:text-gray-500 relative ${
+                    currentRoute === href ? "bg-primary" : ""
+                } rounded-full px-3 py-1.5 font-medium outline-sky-400 transition focus-visible:outline-2`}>
+                    {name}
+                </Link>
+            ))}
         </span>
   )
 }
