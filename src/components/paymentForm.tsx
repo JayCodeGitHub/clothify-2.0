442621ea8490eaf9a1a5489ee113eb88f530c777
JayCode Developer <JayCode.contakt@gmail.com>
2024-01-31 09:42:36 +0100

    Add inputs to PaymentForm component

--- src/components/paymentForm.tsx
+++ src/components/paymentForm.tsx
@@ -1,5 +1,10 @@
 export default function PaymentForm() {
     return (
-        <h2>PaymentForm</h2>
+        <form>
+            <input type="text" placeholder="Cardholder's Name" name="card-name"/>
+            <input type="text" placeholder="Card Number" name="card-number"/>
+            <input type="text" placeholder="MM/YY Expiry Date" name="card-date"/>
+            <input type="text" placeholder="CVV" name="card-cvv"/>
+        </form>
     )
 }
