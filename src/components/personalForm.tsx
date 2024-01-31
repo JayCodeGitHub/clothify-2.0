0ff34a95e629b3b19179c8c7da43a0b58a5c000c
JayCode Developer <JayCode.contakt@gmail.com>
2024-01-31 09:59:24 +0100

    Style Personal Form

--- src/components/personalForm.tsx
+++ src/components/personalForm.tsx
@@ -1,10 +1,22 @@
 export default function PersonalForm() {
     return (
-        <form>
-            <input type="text" placeholder="Full Name" className="w-full h-12 px-4 border border-gray-300 rounded-md my-2" />
-            <input type="text" placeholder="Email" className="w-full h-12 px-4 border border-gray-300 rounded-md my-2" />
-            <input type="text" placeholder="Street, City" className="w-full h-12 px-4 border border-gray-300 rounded-md my-2" />
-            <input type="text" placeholder="Country" className="w-full h-12 px-4 border border-gray-300 rounded-md my-2" />
+        <form className="flex flex-col gap-2">
+            <span className='flex flex-col w-full px-2'>
+                <label className='text-sm self-start'>Full Name</label>
+                <input type="text" placeholder="Full Name" className="w-full p-3 border border-gray-300 rounded-md my-2" />
+            </span>
+            <span className='flex flex-col w-full px-2'>
+                <label className='text-sm self-start'>Email Adress</label>
+                <input type="text" placeholder="Email" className="w-full p-3 border border-gray-300 rounded-md my-2" />
+            </span>
+            <span className='flex flex-col w-full px-2'>
+                <label className='text-sm self-start'>Adress</label>
+                <input type="text" placeholder="Street, City" className="w-full p-3 border border-gray-300 rounded-md my-2" />
+            </span>
+            <span className='flex flex-col w-full px-2'>
+                <label className='text-sm self-start'>Country</label>
+                <input type="text" placeholder="Country" className="w-full p-3 border border-gray-300 rounded-md my-2" />
+            </span>
         </form>
     )
 }
