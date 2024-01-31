70ed860664ae9e6a4a45c0a9e530cd6e13b71df6
JayCode Developer <JayCode.contakt@gmail.com>
2024-01-31 08:31:36 +0100

    Add structure to purchase page

--- src/app/purchase/page.tsx
+++ src/app/purchase/page.tsx
@@ -1,8 +1,11 @@
 export default function Purchase() {
     return (
-        <main>
-            <h1>Purchase</h1>
-      </main>
+        <main className='flex flex-col justify-center items-center w-full h-rest 2xl:px-40 xl:px-28 md:flex-row md:items-start md:gap-4 py-8 px-8 md:px-0'>
+          <span className='md:w-2/5 w-full h-full bg-blue-500'>
+              </span>
+            <div className='md:w-2/5 w-full h-full bg-red-500'>
+            </div>
+        </main>
     )
 }
   
