9bd93f4d39defe34d8d2a5f2dc1c5c421d4a832e
JayCode Developer <JayCode.contakt@gmail.com>
2024-01-31 09:44:55 +0100

    Add labels to paymentForm

--- src/components/paymentForm.tsx
+++ src/components/paymentForm.tsx
@@ -1,9 +1,13 @@
 export default function PaymentForm() {
     return (
         <form>
+            <label>{`Cardholder's Name`}</label>
             <input type="text" placeholder="Cardholder's Name" name="card-name"/>
+            <label>Card Number</label>
             <input type="text" placeholder="Card Number" name="card-number"/>
-            <input type="text" placeholder="MM/YY Expiry Date" name="card-date"/>
+            <label>Expiry Date</label>
+            <input type="text" placeholder="MM/YY" name="card-date"/>
+            <label>CVV</label>
             <input type="text" placeholder="CVV" name="card-cvv"/>
         </form>
     )
