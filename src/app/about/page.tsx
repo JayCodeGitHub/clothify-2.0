f8257464c40151006b922f4710f369632da2ef64
JayCode Developer <JayCode.contakt@gmail.com>
2024-01-31 07:56:32 +0100

    Refactore About Page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,18 +1,25 @@
+const AboutItems = {
+    title: 'About',
+    description: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Sed id augue sed nunc tincidunt aliquam. 
+        Nullam vel quam vitae justo lacinia luctus. Nulla facilisi. 
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Sed id augue sed nunc tincidunt aliquam. 
+        Nullam vel quam vitae justo lacinia luctus. Nulla facilisi.
+   `
+}
+
 export default function About() {
+    const { title, description } = AboutItems;
     return (
         <main className="w-full h-rest">
             <section className="mx-auto mt-16 md:px-0 md:w-1/2">
                 <h1 className="mb-12 text-3xl">
-                    About
+                    {title}
                 </h1>
                 <p className="text-md md:text-xl">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Sed id augue sed nunc tincidunt aliquam.
-                    Nullam vel quam vitae justo lacinia luctus.
-                    Nulla facilisi.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Sed id augue sed nunc tincidunt aliquam.
-                    Nullam vel quam vitae justo lacinia luctus.
-                    Nulla facilisi.
+                    {description}
                 </p>
             </section>
       </main>
