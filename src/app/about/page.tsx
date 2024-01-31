46eb8c7a742a45c200c38161789a46880b66b2dc
JayCode Developer <JayCode.contakt@gmail.com>
2024-01-31 07:53:40 +0100

    Style About Page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,21 @@
 export default function About() {
     return (
-        <main>
-            <h1>About</h1>
-        </main>
+        <main className="w-full h-rest">
+            <section className="mx-auto mt-16 md:px-0 md:w-1/2">
+                <h1 className="mb-12 text-3xl">
+                    About
+                </h1>
+                <p className="text-md md:text-xl">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Sed id augue sed nunc tincidunt aliquam.
+                    Nullam vel quam vitae justo lacinia luctus.
+                    Nulla facilisi.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Sed id augue sed nunc tincidunt aliquam.
+                    Nullam vel quam vitae justo lacinia luctus.
+                    Nulla facilisi.
+                </p>
+            </section>
+      </main>
     )
 }
   
