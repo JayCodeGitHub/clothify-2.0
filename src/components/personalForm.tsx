60ebe9a71dffafb60d611718299e23b98aa0aa37
JayCode Developer <JayCode.contakt@gmail.com>
2024-02-01 06:46:58 +0100

    Move personalFormItems to separate file

--- src/components/personalForm.tsx
+++ src/components/personalForm.tsx
@@ -1,30 +1,4 @@
-const PersonalFormItems = [
-    {
-        label: 'Full Name',
-        placeholder: 'Full Name',
-        type: 'text',
-        name: 'fullName'
-    },
-    {
-        label: 'Email Adress',
-        placeholder: 'Email',
-        type: 'email',
-        name: 'email'
-    },
-    {
-        label: 'Adress',
-        placeholder: 'Street, City',
-        type: 'text',
-        name: 'adress'
-    },
-    {
-        label: 'Country',
-        placeholder: 'Country',
-        type: 'text',
-        name: 'country'
-    },
-
-]
+import { PersonalFormItems } from "@/items/personalFormItems"
 
 export default function PersonalForm() {
     return (
---
+++ src/items/personalFormItems.tsx
@@ -0,0 +1,27 @@
+export const PersonalFormItems = [
+    {
+        label: 'Full Name',
+        placeholder: 'Full Name',
+        type: 'text',
+        name: 'fullName'
+    },
+    {
+        label: 'Email Adress',
+        placeholder: 'Email',
+        type: 'email',
+        name: 'email'
+    },
+    {
+        label: 'Adress',
+        placeholder: 'Street, City',
+        type: 'text',
+        name: 'adress'
+    },
+    {
+        label: 'Country',
+        placeholder: 'Country',
+        type: 'text',
+        name: 'country'
+    },
+
+]
