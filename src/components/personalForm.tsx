0e28baf63b8b70553d89856736d61326dc6c3dbd
JayCode Developer <JayCode.contakt@gmail.com>
2024-02-01 06:45:48 +0100

    Refactore personalForm

--- src/components/personalForm.tsx
+++ src/components/personalForm.tsx
@@ -1,22 +1,40 @@
+const PersonalFormItems = [
+    {
+        label: 'Full Name',
+        placeholder: 'Full Name',
+        type: 'text',
+        name: 'fullName'
+    },
+    {
+        label: 'Email Adress',
+        placeholder: 'Email',
+        type: 'email',
+        name: 'email'
+    },
+    {
+        label: 'Adress',
+        placeholder: 'Street, City',
+        type: 'text',
+        name: 'adress'
+    },
+    {
+        label: 'Country',
+        placeholder: 'Country',
+        type: 'text',
+        name: 'country'
+    },
+
+]
+
 export default function PersonalForm() {
     return (
         <form className="flex flex-col gap-2">
-            <span className='flex flex-col w-full px-2'>
-                <label className='text-sm self-start'>Full Name</label>
-                <input type="text" placeholder="Full Name" className="w-full p-3 border border-gray-300 rounded-md my-2" />
-            </span>
-            <span className='flex flex-col w-full px-2'>
-                <label className='text-sm self-start'>Email Adress</label>
-                <input type="text" placeholder="Email" className="w-full p-3 border border-gray-300 rounded-md my-2" />
-            </span>
-            <span className='flex flex-col w-full px-2'>
-                <label className='text-sm self-start'>Adress</label>
-                <input type="text" placeholder="Street, City" className="w-full p-3 border border-gray-300 rounded-md my-2" />
-            </span>
-            <span className='flex flex-col w-full px-2'>
-                <label className='text-sm self-start'>Country</label>
-                <input type="text" placeholder="Country" className="w-full p-3 border border-gray-300 rounded-md my-2" />
-            </span>
+            {PersonalFormItems.map(({label, type, placeholder, name}, index) => (
+                <span className={`flex flex-col w-full px-2`} key={index}>
+                    <label className='text-sm self-start'>{label}</label>
+                    <input type={type} placeholder={placeholder} name={name} className="w-full p-3 border border-gray-300 rounded-md my-2" />
+                </span>
+            ))}
         </form>
     )
 }
