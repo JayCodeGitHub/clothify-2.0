a7ae83ed36f4c1bd64c47a59908d7e1c39f15b34
JayCode Developer <JayCode.contakt@gmail.com>
2024-02-20 11:52:16 +0100

    Add svg and animation to checkIcon

--- src/components/checkIcon.tsx
+++ src/components/checkIcon.tsx
@@ -1,5 +1,28 @@
-export default function CheckIcon() {
+import { ComponentProps } from "react";
+import { motion } from "framer-motion";
+
+export default function CheckIcon(props: ComponentProps<"svg">) {
   return (
-    <h1>CheckIcon</h1>
+    <svg
+      {...props}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={3}
+    >
+      <motion.path
+        initial={{ pathLength: 0 }}
+        animate={{ pathLength: 1 }}
+        transition={{
+          delay: 0.2,
+          type: "tween",
+          ease: "easeOut",
+          duration: 0.3,
+        }}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M5 13l4 4L19 7"
+      />
+    </svg>
   );
 }
