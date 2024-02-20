1765ceaa5d86a7a938a47e34c8a590227f2bfef4
JayCode Developer <JayCode.contakt@gmail.com>
2024-02-20 12:06:28 +0100

    Add index file to hooks

---
+++ src/hooks/index.tsx
@@ -0,0 +1,7 @@
+import { useAlert } from "./useAlert";
+import { useAuth } from "./useAuth";
+import { useCart } from "./useCart";
+import { useError } from "./useError";
+import { useItems } from "./useItems";
+
+export { useAlert, useAuth, useCart, useError, useItems };
