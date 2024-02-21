ca13514e4b6abb1a48626c96008b2be571b70c23
JayCode Developer <JayCode.contakt@gmail.com>
2024-02-21 07:19:41 +0100

    Remove unused items from purchase page

--- src/app/purchase/page.tsx
+++ src/app/purchase/page.tsx
@@ -1,71 +1,12 @@
 "use client"
 
-import { useState } from "react";
 import { useCart } from "@/hooks/useCart"
 import CartItem from "@/components/cartItem";
-import Button from "@/components/button";
 import PurchaseForm from "@/components/purchaseForm";
 
 
 export default function Purchase() {
     const { cart } = useCart();
-
-    const initialForm = {
-        fullName: '',
-        email: '',
-        address: '',
-        country: '',
-        cardName: '',
-        cardNumber: '',
-        cardDate: '',
-        cardCvv: '',
-    }
-    
-    const [form, setForm] = useState({ ...initialForm})
-    const [error, setError] = useState<null | string>(null)
-
-    function subtotal() {
-        let current = 0;
-        cart.map((item) => (current += item.price * item.quantity));
-        return current;
-    }
-
-    const validate = () => {
-        const { 
-            fullName,
-            email,
-            address,
-            country,
-            cardName,
-            cardNumber,
-            cardDate,
-            cardCvv
-        } = form;
-        if(!fullName || !email || !address || !country || !cardName || !cardNumber || !cardDate || !cardCvv) {
-            setError('All fields are required');
-            return false;
-        }
-        setError(null);
-        return true;
-    }
-
-    const updateField = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setForm({
-            ...form,
-            [e.target.name]: e.target.value
-        })
-    }
-    
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const isValid = validate();
-        if(isValid) {
-            console.log(form);
-            setForm({...initialForm});
-            e.currentTarget.reset();
-        }
-    }
-
     return (
         <main className='flex flex-col justify-center items-center w-full min-h-rest 2xl:px-40 xl:px-28 md:flex-row md:items-start md:gap-4 py-8 px-8 md:px-2'>
             <section className='xl:w-2/5 w-full h-full'>
@@ -86,9 +27,7 @@ export default function Purchase() {
                 </div>
             </section>
             <section className='xl:w-2/5 w-full h-full pt-10'>
-                <PurchaseForm />
-                <p className="pt-4 my-4 font-medium">Subtotal: {subtotal()}$</p>
-                <Button type="submit">Order Now</Button>
+            <PurchaseForm />
             </section>
         </main>
     )
