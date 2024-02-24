ba8b3743877d1c243f05a17472a9d3c9f86d556e
JayCode Developer <JayCode.contakt@gmail.com>
2024-02-24 10:07:42 +0100

    Add Providers to hooks index file

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,8 +1,8 @@
-import { useAlert } from "./useAlert";
-import { useAuth } from "./useAuth";
-import { useCart } from "./useCart";
+import { useAlert, AlertProvider } from "./useAlert";
+import { useAuth, AuthProvider } from "./useAuth";
+import { useCart, CartProvider } from "./useCart";
 import { useError } from "./useError";
-import { useItems } from "./useItems";
+import { useItems, ItemsProvider } from "./useItems";
 import { useMultiStepForm } from "./useMultiStepForm";
 
-export { useAlert, useAuth, useCart, useError, useItems, useMultiStepForm };
+export { useAlert, useAuth, useCart, useError, useItems, useMultiStepForm, AlertProvider, AuthProvider, CartProvider, ItemsProvider};
