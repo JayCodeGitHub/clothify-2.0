f12613d7faf2e61c38811f5df616271a5d4a0ab5
JayCode Developer <JayCode.contakt@gmail.com>
2024-03-04 20:15:11 +0100

    Add regex, errorRequire, errorRegex fields to formItems

--- src/items/formItems.tsx
+++ src/items/formItems.tsx
@@ -4,11 +4,17 @@ export const formItems = [
       type: 'text',
       label: 'Email address',
       placeholder: 'Your Email Address',
+      regex: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/,
+      errorRequire: 'Email is required',
+      errorRegex: 'Invalid email',
     },
     {
       name: 'password',
       type: 'password',
       label: 'Password',
       placeholder: 'Password',
+      regex: /^.{7,}$/,
+      errorRequire: 'Password is required',
+      errorRegex: 'Password must be at least 6 characters',
     },
 ]
