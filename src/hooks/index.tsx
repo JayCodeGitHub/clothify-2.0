cf7c13c91c8f907c682463487c43da60db9b0432
JayCode Developer <JayCode.contakt@gmail.com>
2024-03-05 06:54:35 +0100

    Remove useError hook

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,7 +1,6 @@
 import { useAlert, AlertProvider } from "./useAlert";
 import { useAuth, AuthProvider } from "./useAuth";
 import { useCart, CartProvider } from "./useCart";
-import { useError } from "./useError";
 import { useItems, ItemsProvider } from "./useItems";
 import { useForm } from "./useForm";
 
@@ -9,7 +8,6 @@ export {
     useAlert,
     useAuth,
     useCart,
-    useError,
     useItems,
     useForm,
     AlertProvider,
