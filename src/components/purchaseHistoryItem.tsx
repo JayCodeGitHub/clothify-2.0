524519d296c770e8198d3923228f8a50f0fffae3
JayCode Developer <JayCode.contakt@gmail.com>
2024-03-06 16:44:10 +0100

    Remove PurchaseHistoryItem

--- src/components/purchaseHistoryItem.tsx
+++
@@ -1,7 +0,0 @@
-export default function PurchaseHistoryItem() {
-    return (
-        <div className="flex justify-start items-center gap-8 p-4 w-full bg-slate-100 rounded-lg flex-row">
-          PurchaseHistoryItem
-        </div>
-    )
-}
