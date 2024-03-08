832aebcfd9d7f28bc98f865cb02cf6e0f242ba31
JayCode Developer <JayCode.contakt@gmail.com>
2024-03-08 07:54:50 +0100

    Fix purchase page h

--- src/app/purchase/page.tsx
+++ src/app/purchase/page.tsx
@@ -8,7 +8,7 @@ import PurchaseForm from "@/components/purchaseForm";
 export default function Purchase() {
     const { cart } = useCart();
     return (
-        <main className='flex flex-col justify-center items-center w-full min-h-rest 2xl:px-40 xl:px-28 md:flex-row md:items-start md:gap-4 py-8 px-8 md:px-2 h-restPhone md:h-restDesktop'>
+        <main className='flex flex-col justify-center items-center w-full min-h-rest 2xl:px-40 xl:px-28 md:flex-row md:items-start md:gap-4 py-8 px-8 md:px-2 min-h-[91vh]'>
             <section className='xl:w-2/5 w-full h-full'>
                 <h1 className="text-xl mx-12 my-8 font-medium">Your order</h1>
                 <div className="flex flex-col w-full gap-8 items-center 2xl:px-12 xl:px-6 md:px-2 px-16">
