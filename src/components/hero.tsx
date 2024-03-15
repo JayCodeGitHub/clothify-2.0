7b0718365b5667f73d4bfa58957d3c6a09ee4dd8
JayCode Developer <JayCode.contakt@gmail.com>
2024-03-15 14:23:54 +0100

    Style Hero for Mobile

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -9,14 +9,14 @@ const MotionImage = motion(Image)
 
 export default function Hero() {
   return (
-    <section className='px-40 py-10 w-full h-restDesktop relative'>
+    <section className='2xl:px-44 xl:px-28 px-4 py-10 w-full h-restDesktop relative'>
       <div className='w-full h-full z-0 overflow-hidden rounded-3xl'>
         <MotionImage
           src="/images/hero.jpg"
           width="3500"
           height="2333"
           alt="Hero Image"
-          className='w-full'
+          className='w-full h-full object-cover'
           initial={{ scale: 1.2}}
           animate={{ scale: 1}}
           transition={{ duration: 0.5}}
@@ -26,12 +26,12 @@ export default function Hero() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ delay: 0.5, duration: 0.5 }}
-      className='z-10 absolute top-1/2 -translate-y-1/2 left-80 max-w-xl flex flex-col gap-4'
+      className='z-10 absolute top-1/2 -translate-y-1/2 2xl:left-80 left-12 xl:left-64 xl:max-w-xl max-w-sm flex flex-col gap-4'
     >
-      <h1 className='text-5xl font-bold'>
+      <h1 className='2xl:text-5xl xl:text-4xl text-3xl font-bold'>
         Unleash Innovation in Every Byte.
       </h1>
-      <h2 className='text-3xl font-semibold'>
+      <h2 className='2xl:text-3xl xl:text-xl text-lg font-semibold'>
         Explore a World of Cutting-Edge Technology.
       </h2>
       <div className='w-32 mt-8'>
