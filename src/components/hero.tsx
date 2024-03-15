7acfd272e14a6316886b3fa5a362a44141fd6f3b
JayCode Developer <JayCode.contakt@gmail.com>
2024-03-15 13:32:31 +0100

    Add hero image intro animation

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,17 +1,27 @@
+"use client";
+
 import Image from 'next/image'
 import Button from './button'
 import NavLink from './navlink'
+import { motion } from 'framer-motion'
+
+const MotionImage = motion(Image)
 
 export default function Hero() {
   return (
-    <section>
-    <Image
-      src="/images/hero.jpg"
-      width="3500"
-      height="2333"
-      alt="Hero Image"
-    />
-    <header>
+    <section className='px-40 py-10 w-full h-restDesktop relative'>
+      <div className='w-full h-full z-0 overflow-hidden rounded-3xl'>
+        <MotionImage
+          src="/images/hero.jpg"
+          width="3500"
+          height="2333"
+          alt="Hero Image"
+          className=' object-cover w-full h-full z-0 rounded-3xl'
+          initial={{ scale: 1.1}}
+          animate={{ scale: 1}}
+        />
+      </div>      
+    <header className='z-10 absolute bg-red-500 top-1/2'>
       <h1>
         Unleash Innovation in Every Byte.
       </h1>
