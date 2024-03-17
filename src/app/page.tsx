97c7e3a3f6894db443c46523a31596c2e34782f0
JayCode Developer <JayCode.contakt@gmail.com>
2024-03-17 14:15:56 +0100

    Add ProductExamples to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/hero';
 import Features from '@/components/features';
+import ProductExamples from '@/components/productExamples';
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Features />
+      <ProductExamples />
     </main>
   )
 }
