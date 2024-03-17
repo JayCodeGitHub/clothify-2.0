0961f1c1d5c0344d98378dbd3f678c08b39b26a1
JayCode Developer <JayCode.contakt@gmail.com>
2024-03-17 14:04:33 +0100

    Style Features component

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -2,15 +2,15 @@ import { FeaturesItems } from "@/items/FeaturesItems"
 
 export default function Features() {
   return (
-    <section>
-      <div>
-        {FeaturesItems.map((item, index) => (
-          <div key={index}>
-            <div>
-              {item.icon}
+    <section className="backdrop-blur-md tracking-wide bg-primary/5">
+      <div className="2xl:px-52 xl:px-32 px-16 py-10 md:grid md:grid-cols-2 lg:grid-cols-4 grid-cols-1 md:gap-12 md:space-y-0 space-y-24">
+        {FeaturesItems.map(({icon, title, description}, i) => (
+          <div key={i} className="flex flex-col items-start gap-2">
+            <div  className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900 text-primary">
+              {icon}
             </div>
-            <h3>{item.title}</h3>
-            <p>{item.description}</p>
+            <h3 className="mb-2 text-xl font-bold text-light">{title}</h3>
+            <p className="text-gray-400">{ description}</p>
           </div>
         ))}
       </div>
