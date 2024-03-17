c6e40003233640acdab8bdf7068bc80afc0c984b
JayCode Developer <JayCode.contakt@gmail.com>
2024-03-17 12:04:10 +0100

    Create FeaturesItems file

---
+++ src/items/FeaturesItems.tsx
@@ -0,0 +1,17 @@
+export const FeaturesItems = [
+    {
+        icon: 'Icon',
+        title: 'Title',
+        description: 'description'
+    },
+    {
+        icon: 'Icon',
+        title: 'Title',
+        description: 'description'
+    },
+    {
+        icon: 'Icon',
+        title: 'Title',
+        description: 'description'
+    }
+]
