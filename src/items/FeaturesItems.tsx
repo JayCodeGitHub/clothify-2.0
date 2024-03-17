aa57ef00b380f2bd5db6efa50a05062d55f559da
JayCode Developer <JayCode.contakt@gmail.com>
2024-03-17 14:04:14 +0100

    Add FeaturesItems

--- src/items/FeaturesItems.tsx
+++ src/items/FeaturesItems.tsx
@@ -1,17 +1,22 @@
 export const FeaturesItems = [
     {
-        icon: 'Icon',
-        title: 'Title',
-        description: 'description'
+        icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="2" y="4" width="20" height="16" rx="2"/><path d="M7 15h0M2 9.5h20"/></svg>,
+        title: 'Easy payment',
+        description: `Seamlessly checkout with our hassle-free payment options, making your shopping experience a breeze.`
     },
     {
-        icon: 'Icon',
-        title: 'Title',
-        description: 'description'
+        icon:  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="1" x2="12" y2="23"></line><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path></svg>,
+        title: 'Best prices',
+        description: `Unlock unbeatable deals and savings on a wide range of products, ensuring you get the best value for your money.`
     },
     {
-        icon: 'Icon',
-        title: 'Title',
-        description: 'description'
+        icon: <svg width="48px" height="48px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-labelledby="transportIconTitle" stroke="currentColor" stroke-width="1" stroke-linecap="square" stroke-linejoin="miter" color="currentColor"> <title id="transportIconTitle">Transport</title> <path d="M4 17H2V7H15V17H8"/> <path d="M20 17H22V12.5556L20 9H15V17H16"/> <circle cx="6" cy="17" r="2"/> <circle cx="18" cy="17" r="2"/> </svg>,
+        title: 'Fast delivery',
+        description: `Lightning-fast shipping to your doorstep, because we know you can't wait to get your hands on your latest purchase.`
+    },
+    {
+        icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>,
+        title: '24/7 Support',
+        description: `Expert assistance is just a click or call away, ensuring you're never left in the dark with any queries or concerns, no matter the time.`
     }
 ]
