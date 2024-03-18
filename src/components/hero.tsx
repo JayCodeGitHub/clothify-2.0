38f5dce31eb2d95d88b8de9ad278d06ec0560b8a
JayCode Developer <JayCode.contakt@gmail.com>
2024-03-18 09:47:11 +0100

    Update hero style

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -10,7 +10,7 @@ const MotionImage = motion(Image)
 export default function Hero() {
   return (
     <section className='2xl:px-44 xl:px-28 px-4 py-10 w-full h-restDesktop relative'>
-      <div className='w-full h-full z-0 overflow-hidden rounded-3xl'>
+      <div className='w-full h-full z-0 overflow-hidden rounded-3xl blur-[3px]'>
         <MotionImage
           src="/images/hero.jpg"
           width="3500"
@@ -28,10 +28,10 @@ export default function Hero() {
       transition={{ delay: 0.5, duration: 0.5 }}
       className='z-10 absolute top-1/2 -translate-y-1/2 2xl:left-80 left-12 xl:left-64 xl:max-w-xl max-w-sm flex flex-col gap-4'
     >
-      <h1 className='2xl:text-5xl xl:text-4xl text-3xl font-bold'>
+      <h1 className='2xl:text-5xl xl:text-4xl text-3xl font-bold text-primary'>
         Unleash Innovation in Every Byte.
       </h1>
-      <h2 className='2xl:text-3xl xl:text-xl text-lg font-semibold'>
+      <h2 className='2xl:text-3xl xl:text-xl text-lg font-semibold text-gray-700'>
         Explore a World of Cutting-Edge Technology.
       </h2>
       <div className='w-32 mt-8'>
