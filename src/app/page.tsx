c29385413a0428cb7e7c3b50a5130290151dbef2
JayCode Developer <JayCode.contakt@gmail.com>
2024-03-18 07:16:19 +0100

    Add items to productExamples

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import Hero from '@/components/hero';
 import Features from '@/components/features';
 import ProductExamples from '@/components/productExamples';
+import { PAGE_CONTENT_QUERY } from '@/lib/queries';
+import { performRequest } from '../lib/datocms';
 
-export default function Home() {
+export default async function Home() {
+  const { data: { allItems } } = await performRequest({ query: PAGE_CONTENT_QUERY });
+  const items = allItems.slice(0, 3);
   return (
     <main>
       <Hero />
       <Features />
-      <ProductExamples />
+      <ProductExamples productExamples={items}/>
     </main>
   )
 }
--- src/components/productExamples.tsx
+++ src/components/productExamples.tsx
@@ -1,7 +1,18 @@
-export default function productExamples() {
+import Item from "./item";
+import { ItemType } from '@/types';
+
+interface productExampleProps {
+  productExamples: ItemType[]
+}
+
+export default function productExamples({productExamples}: productExampleProps) {
   return (
-   <h1>
-        ProductExamples
-   </h1>
+    <section className='my-14'>
+    <ul className='grid 2xl:px-40 xl:px-28 px-4 2xl:grid-cols-4 md:grid-cols-3 grid-cols-2 lg:gap-y-20 gap-y-14 gap-x-4 lg:gap-x-0'>
+      {productExamples.map((item) => (
+        <Item key={item.id} item={item}/>
+      ))}
+    </ul>
+  </section>
   )
 }
