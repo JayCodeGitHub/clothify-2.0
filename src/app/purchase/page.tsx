c7d3bf25d2a7d0e68ad7709caef581f057a4f0a5
JayCode Developer <JayCode.contakt@gmail.com>
2024-03-19 06:39:08 +0100

    Fix Purchase Page Items padding

--- src/app/purchase/page.tsx
+++ src/app/purchase/page.tsx
@@ -11,7 +11,7 @@ export default function Purchase() {
         <main className='flex flex-col justify-center items-center w-full min-h-rest 2xl:px-40 xl:px-28 md:flex-row md:items-start md:gap-4 py-8 px-8 md:px-2 min-h-[91vh]'>
             <section className='xl:w-2/5 w-full h-full'>
                 <h1 className="text-xl mx-12 my-8 font-medium">Your order</h1>
-                <div className="flex flex-col w-full gap-8 items-center 2xl:px-12 xl:px-6 md:px-2 px-16">
+                <div className="flex flex-col w-full gap-8 items-center 2xl:px-12 xl:px-6 md:px-2 px-8">
                     {cart.map((item) => (
                         <CartItem
                             name="PurchaseItem"
