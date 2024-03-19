f31a7331e527859594a38a592ca7f509c0352123
JayCode Developer <JayCode.contakt@gmail.com>
2024-03-19 06:43:30 +0100

    Fix about page height

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,7 +8,7 @@ export default async function About() {
      
     const { title, description } = about;
     return (
-        <main className="w-full h-restPhone md:h-restDesktop">
+        <main className="w-full min-h-restPhone md:h-restDesktop">
             <section className="mx-auto pt-16 2xl:px-56 xl:px-44 px-4">
                 <h1 className="pb-12 text-3xl">
                     {title}
